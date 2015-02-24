32676e7e47f8f6841b26046e46fe6e20a639e141
joseacabaneros <joseacabaneros@gmail.com>
2015-02-24 19:04:10 +0100

    Parser XML to JSON

--- extract/src/main/java/es/uniovi/asw/trivial/Extractor.java
+++ extract/src/main/java/es/uniovi/asw/trivial/Extractor.java
@@ -1,19 +1,22 @@
 package es.uniovi.asw.trivial;
 
+
 public class Extractor {
-	
+
 	public void usage() {
 		System.out.println("Wellcome to Trivial Extractor");
+		new ParserXMLtoJSON("files/preguntasXML.xml");
 	}
+
 	public int run(String[] args) {
 		if (args.length == 0) {
 			usage();
 			return 0;
 		}
-		return -1 ;
+		return -1;
 	}
-	
+
 	public static void main(String[] args) {
-        new Extractor().run(args);
-    }	
+		new Extractor().run(args);
+	}
 }
