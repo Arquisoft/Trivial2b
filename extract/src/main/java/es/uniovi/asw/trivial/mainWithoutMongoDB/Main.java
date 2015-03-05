5ce2050be9310afae04eb39a15cf45f8cccc14ea
carlosord <carlos.ord.gon@gmail.com>
2015-03-05 15:54:11 +0100

    Ejecucion del programa
    Modificacion de los parser y cambio en la ejecucion del programa

--- extract/src/main/java/es/uniovi/asw/trivial/mainWithoutMongoDB/Main.java
+++ extract/src/main/java/es/uniovi/asw/trivial/mainWithoutMongoDB/Main.java
@@ -1,9 +1,9 @@
 package es.uniovi.asw.trivial.mainWithoutMongoDB;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-
-import com.google.gson.Gson;
 
 import es.uniovi.asw.trivial.juego.JuegoConsola;
 import es.uniovi.asw.trivial.model.Pregunta;
@@ -12,16 +12,26 @@ import es.uniovi.asw.trivial.parser.ParserGIFT;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Introduzca la ruta del fichero: ");
+		String ruta = entrada.readLine();
+		
 		//parseadores
-		ParserGIFT parser = new ParserGIFT();
-		Gson gson = new Gson();
+		ParserGIFT gift;
+//		ParserXMLtoJSON xml;
+//		ParserToJSON json;
 		
-		//preguntas
-		ArrayList<Pregunta> preguntas = parser.parsear("files/preguntasGIFT.txt");
+		//Preguntas
+		ArrayList<Pregunta> preguntas;
+		if (ruta.toLowerCase().endsWith(".gift")) {
+			gift = new ParserGIFT();
+			preguntas = gift.parsear(ruta);
+		} else {
+			preguntas = new ArrayList<Pregunta>();
+		}
 		
-		
-		//System.out.println(gson.toJson(p) + "\n");
-		
+		//Juego				
 		JuegoConsola juegoConsola = new JuegoConsola(preguntas);
 		juegoConsola.jugar();
 		
---
+++ extract/src/main/java/es/uniovi/asw/trivial/parser/ParserToJSON.java
@@ -0,0 +1,42 @@
+package es.uniovi.asw.trivial.parser;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+import es.uniovi.asw.trivial.model.Pregunta;
+
+public class ParserToJSON {
+	
+	Gson gson;
+	
+	public ParserToJSON() {
+		gson = new Gson();
+	}
+	
+	private ArrayList<String> toJson(ArrayList<Pregunta> preguntas) {
+		ArrayList<String> preguntasJson = new ArrayList<>();
+		for (Pregunta p: preguntas)
+			preguntasJson.add(gson.toJson(p));
+		return preguntasJson;
+	}
+	
+	public void generateJson(ArrayList<Pregunta> preguntas) throws IOException {
+		
+		File file = new File("files/preguntasFromIntermedio.json");
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		ArrayList<String> json = toJson(preguntas);
+		
+		String fichero = "";
+		for (String s: json)
+			fichero += s + "\n";
+		bw.write(fichero);
+		bw.close();
+	}
+
+}
