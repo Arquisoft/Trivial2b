54e9e856d377d254da69058b1868545288e78e62
Jaime Díaz García <jimmydg94@gmail.com>
2015-04-10 00:03:16 +0200

    Base datos users

---
+++ extract/src/main/java/es/uniovi/asw/trivial/persistence/exception/NoExisteUsuarioException.java
@@ -0,0 +1,10 @@
+package es.uniovi.asw.trivial.persistence.exception;
+
+public class NoExisteUsuarioException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
