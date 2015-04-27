f02331911681fcc06ba9e97c5b634942bc15d6b3
Adrián <adri.arenal94@gmail.com>
2015-04-27 18:54:12 +0200

    Nuevas vistas y navegabilidad

--- web-app/app/controllers/Application.java
+++ web-app/app/controllers/Application.java
@@ -1,8 +1,8 @@
 package controllers;
 
 import play.*;
+import play.data.Form;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
@@ -10,5 +10,21 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("asdsaddYour new application is ready."));
     }
+    
+    public static Result showRegister() {
+		return ok(register.render(Form.form(Register.class)));
+	}
+    
+    public static Result showEstadisticas(){
+    	return ok(estadisticas.render());
+    }
+    public static Result showAdmin(){
+    	return ok(admin.render());
+    }
+    public static class Register {
+		public String id;
+		public String password;
+		public String password2;
 
+	}
 }
